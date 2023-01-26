d323632f4a941d32ead4fbfcbcd97eaedbfc5fdb
Ruben Montes <rubenmontes330@gmail.com>
2023-01-26 12:10:12 +0000

    customHook

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {Component, useState} from 'react'
+import {useState} from 'react'
 
 //Reglas d elos hooks
 
@@ -9,31 +9,39 @@ import {Component, useState} from 'react'
 //                             -> Custom Hooks
 
 //Cuando creamos un customHooks  siempre el nombre empiexa por use+"Algo"
-class App extends Component {
-    state={
-      contador:0
-    }
-    incrementar= () => {
-      this.setState({ contador: this.state.contador+1})
-    }
-    render() {
-      return(
-         <div>
-          Contador: {this.state.contador}
-          <button onClick={this.incrementar}> Incementar</button>
-    </div>
-      )
-    }
+// // // // class App extends Component {
+// // // //     state={
+// // // //       contador:0
+// // // //     }
+// // // //     incrementar= () => {
+// // // //       this.setState({ contador: this.state.contador+1})
+// // // //     }
+// // // //     render() {
+// // // //       return(
+// // // //          <div>
+// // // //           Contador: {this.state.contador}
+// // // //           <button onClick={this.incrementar}> Incementar</button>
+// // // //     </div>
+// // // //       )
+// // // //     }
+// // // // }
+
+const useContador = (inicial) => {
+  const [contador, setContador] = useState(inicial)
+  const incrementar =() => {
+    setContador(contador+1)
+  }
+  return [contador, incrementar]
 }
-// const App= () => {
+const App= () => {
 
-//   const [contador, setContador] = useState(0)
+  const[contador, incrementar] = useContador(0)
 
-//   return (
-//     <div>
-//           Contador: {contador}
-//           <button onClick={() => setContador( contador +1)}> Incementar</button>
-//     </div>
-//   )
-// }
+  return (
+    <div>
+          Contador: {contador}
+          <button onClick= {incrementar}> Incementar</button>
+    </div>
+  )
+}
 export default App;
