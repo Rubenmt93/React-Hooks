65b624e2416b5bdb50653b582361f2ae10c71454
Ruben Montes <rubenmontes330@gmail.com>
2023-01-30 19:49:58 +0000

    desuscricrise  a un UseEffect

--- src/App.js
+++ src/App.js
@@ -5,22 +5,30 @@ const useContador = (inicial) => {
       setContador(contador+1)
     }
     return [contador, incrementar]
-  }
+}
+const Interval= ({contador}) => {
+  useEffect(()=>{
+    const i = setInterval(()=>console.log(contador),1000)
+    return () =>clearInterval(i)  
+  },[contador])
+  return (
+    <p> Intervalo</p>
+  )
+}
+const App= () => {
 
-  const App= () => {
-  
-    const[contador, incrementar] = useContador(0)
-    useEffect(()=>{ 
-        document.title=contador
-        console.log('bu')
-      },
-      [contador])
-    return (
-      <div>
-            Contador: {contador}
-            <button onClick= {incrementar}> Incementar</button>
-      </div>
-    )
-  }
-  export default App;
-  
+  const[contador, incrementar] = useContador(0)
+  useEffect(()=>{ 
+      document.title=contador
+      console.log('bu')
+    },
+    [contador])
+  return (
+    <div>
+          Contador: {contador}
+          <button onClick= {incrementar}> Incementar</button>
+          <Interval contador= {contador}></Interval>
+    </div>
+  )
+}
+export default App;
